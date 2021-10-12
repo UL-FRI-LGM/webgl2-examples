0b1af7306e3f012702494bc06fd6a57fc40ca99d
Žiga Lesar <terier.kuza@gmail.com>
2021-10-12 14:58:05 +0200

    Platform independence: path.sep

--- bin/server.js
+++ bin/server.js
@@ -20,7 +20,7 @@ const server = http.createServer(async (req, res) => {
     // get a file path from the project root
     const rootPath = path.join(__dirname, '..');
     let filePath = path.join(rootPath, req.url);
-    if (filePath.endsWith('/')) {
+    if (filePath.endsWith(path.sep)) {
         filePath = path.join(filePath, 'index.html');
     }
 
