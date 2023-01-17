5d76608a7e66ca745367c0fa72780c073debef05
Žiga Lesar <terier.kuza@gmail.com>
2023-01-17 13:32:21 +0100

    Fix SceneLoader relative URLs

--- examples/04-collision/01-aabb-aabb/SceneLoader.js
+++ examples/04-collision/01-aabb-aabb/SceneLoader.js
@@ -1,9 +1,12 @@
 export class SceneLoader {
 
     async loadScene(url) {
-        const scene = await this.loadJson(url);
-        const images = scene.textures.map(url => this.loadImage(url));
-        const meshes = scene.meshes.map(url => this.loadJson(url));
+        const sceneUrl = new URL(url, window.location);
+        const scene = await this.loadJson(sceneUrl);
+
+        const images = scene.textures.map(url => this.loadImage(new URL(url, sceneUrl)));
+        const meshes = scene.meshes.map(url => this.loadJson(new URL(url, sceneUrl)));
+
         scene.textures = await Promise.all(images);
         scene.meshes = await Promise.all(meshes);
 
