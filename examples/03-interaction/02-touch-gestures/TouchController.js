5183e0af5570e95fd92839b3eb777eef9b494ac7
Žiga Lesar <terier.kuza@gmail.com>
2024-01-27 14:15:42 +0100

    Add TouchController

---
+++ examples/03-interaction/02-touch-gestures/TouchController.js
@@ -0,0 +1,143 @@
+import { quat, vec2, vec3 } from '../../../lib/gl-matrix-module.js';
+
+import { Transform } from '../../../common/engine/core/Transform.js';
+
+export class TouchController {
+
+    constructor(node, domElement, {
+        translation = [0, 0, 0],
+        rotation = [0, 0, 0, 1],
+        distance = 2,
+        translateSensitivity = 0.001,
+        rotateSensitivity = 0.004,
+        wheelSensitivity = 0.002,
+    } = {}) {
+        this.node = node;
+        this.domElement = domElement;
+
+        this.translation = translation;
+        this.rotation = rotation;
+        this.distance = distance;
+
+        this.translateSensitivity = translateSensitivity;
+        this.rotateSensitivity = rotateSensitivity;
+        this.wheelSensitivity = wheelSensitivity;
+
+        this.pointers = new Map();
+        this.initHandlers();
+    }
+
+    initHandlers() {
+        this.pointerdownHandler = this.pointerdownHandler.bind(this);
+        this.pointerupHandler = this.pointerupHandler.bind(this);
+        this.pointermoveHandler = this.pointermoveHandler.bind(this);
+        this.wheelHandler = this.wheelHandler.bind(this);
+
+        this.domElement.addEventListener('pointerdown', this.pointerdownHandler);
+        this.domElement.addEventListener('pointerup', this.pointerupHandler);
+        this.domElement.addEventListener('pointercancel', this.pointerupHandler);
+        this.domElement.addEventListener('pointermove', this.pointermoveHandler);
+        this.domElement.addEventListener('wheel', this.wheelHandler);
+    }
+
+    pointerdownHandler(e) {
+        this.pointers.set(e.pointerId, e);
+        this.domElement.setPointerCapture(e.pointerId);
+        this.domElement.requestPointerLock();
+    }
+
+    pointerupHandler(e) {
+        this.pointers.delete(e.pointerId);
+        this.domElement.releasePointerCapture(e.pointerId);
+        this.domElement.ownerDocument.exitPointerLock();
+    }
+
+    pointermoveHandler(e) {
+        if (!this.pointers.has(e.pointerId)) {
+            return;
+        }
+
+        if (this.pointers.size === 1) {
+            if (e.shiftKey) {
+                this.translate(e.movementX, e.movementY);
+            } else {
+                this.rotate(e.movementX, e.movementY);
+            }
+            this.pointers.set(e.pointerId, e);
+        } else {
+            const N = this.pointers.size;
+
+            // Points before movement
+            const A = [...this.pointers.values()].map(e => [e.clientX, e.clientY]);
+
+            this.pointers.set(e.pointerId, e);
+
+            // Points after movement
+            const B = [...this.pointers.values()].map(e => [e.clientX, e.clientY]);
+
+            const centroidA = A.reduce((a, v) => vec2.scaleAndAdd(a, a, v, 1 / N), [0, 0]);
+            const centroidB = B.reduce((a, v) => vec2.scaleAndAdd(a, a, v, 1 / N), [0, 0]);
+
+            const translation = vec2.subtract(vec2.create(), centroidB, centroidA);
+
+            const centeredA = A.map(v => vec2.subtract(vec2.create(), v, centroidA));
+            const centeredB = B.map(v => vec2.subtract(vec2.create(), v, centroidB));
+
+            const scaleA = centeredA.reduce((a, v) => a + vec2.length(v) / N, 0);
+            const scaleB = centeredB.reduce((a, v) => a + vec2.length(v) / N, 0);
+
+            const scale = scaleA / scaleB;
+
+            const normalizedA = centeredA.map(v => vec2.normalize(vec2.create(), v));
+            const normalizedB = centeredB.map(v => vec2.normalize(vec2.create(), v));
+
+            let sin = 0;
+            for (let i = 0; i < A.length; i++) {
+                const a = normalizedA[i];
+                const b = normalizedB[i];
+                sin += (a[0] * b[1] - a[1] * b[0]) / N;
+            }
+            const angle = Math.asin(sin);
+
+            this.translate(...translation);
+            this.scale(scale);
+            this.screw(angle);
+        }
+    }
+
+    wheelHandler(e) {
+        this.scale(Math.exp(e.deltaY * this.wheelSensitivity));
+    }
+
+    screw(amount) {
+        quat.rotateZ(this.rotation, this.rotation, amount);
+    }
+
+    rotate(dx, dy) {
+        quat.rotateX(this.rotation, this.rotation, -dy * this.rotateSensitivity);
+        quat.rotateY(this.rotation, this.rotation, -dx * this.rotateSensitivity);
+        quat.normalize(this.rotation, this.rotation);
+    }
+
+    translate(dx, dy) {
+        const translation = [-dx, dy, 0];
+        vec3.transformQuat(translation, translation, this.rotation);
+        vec3.scaleAndAdd(this.translation, this.translation, translation, this.distance * this.translateSensitivity);
+    }
+
+    scale(amount) {
+        this.distance *= amount;
+    }
+
+    update() {
+        const transform = this.node.getComponentOfType(Transform);
+        if (!transform) {
+            return;
+        }
+
+        quat.copy(transform.rotation, this.rotation);
+        vec3.transformQuat(transform.translation, [0, 0, this.distance], this.rotation);
+        vec3.add(transform.translation, transform.translation, this.translation);
+    }
+
+}
---
+++ examples/03-interaction/02-touch-gestures/main.js
@@ -0,0 +1,79 @@
+import { mat4 } from '../../../lib/gl-matrix-module.js';
+
+import {
+    Camera,
+    Material,
+    Model,
+    Node,
+    Primitive,
+    Sampler,
+    Texture,
+    Transform,
+} from '../../../common/engine/core.js';
+
+import * as WebGL from '../../../common/engine/WebGL.js';
+import { ResizeSystem } from '../../../common/engine/systems/ResizeSystem.js';
+import { UpdateSystem } from '../../../common/engine/systems/UpdateSystem.js';
+import { ImageLoader } from '../../../common/engine/loaders/ImageLoader.js';
+import { JSONLoader } from '../../../common/engine/loaders/JSONLoader.js';
+import { UnlitRenderer } from '../../../common/engine/renderers/UnlitRenderer.js';
+
+import { TouchController } from './TouchController.js';
+
+const canvas = document.querySelector('canvas');
+const renderer = new UnlitRenderer(canvas);
+await renderer.initialize();
+
+const scene = new Node();
+
+const camera = new Node();
+camera.addComponent(new Transform({
+    translation: [0, 1, 0],
+}));
+camera.addComponent(new Camera({
+    near: 0.01,
+    far: 1000,
+}));
+camera.addComponent(new TouchController(camera, canvas, { distance: 5 }));
+scene.addChild(camera);
+
+const floor = new Node();
+floor.addComponent(new Transform());
+floor.addComponent(new Model({
+    primitives: [
+        new Primitive({
+            mesh: await new JSONLoader().loadMesh('../../../common/models/cube.json'),
+            material: new Material({
+                baseTexture: new Texture({
+                    image: await new ImageLoader().load('../../../common/images/grass.png'),
+                    sampler: new Sampler({
+                        minFilter: 'nearest',
+                        magFilter: 'nearest',
+                    }),
+                }),
+            }),
+        }),
+    ],
+}));
+scene.addChild(floor);
+
+function update(time, dt) {
+    scene.traverse(node => {
+        for (const component of node.components) {
+            component.update?.(time, dt);
+        }
+    });
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function resize({ displaySize: { width, height }}) {
+    camera.getComponentOfType(Camera).aspect = width / height;
+}
+
+new ResizeSystem({ canvas, resize }).start();
+new UpdateSystem({ update, render }).start();
+
+document.querySelector('.loader-container').remove();
